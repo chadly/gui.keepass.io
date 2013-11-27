ad743c05cc8057820a6b46a21f38609e37fc8595
Chad Lee <git@chadly.net>
2013-11-27 10:50:23 -0600

    Splitting server app definition and running of server.

---
+++ src/server/app.js
@@ -0,0 +1,50 @@
+﻿var express = require("express"),
+	http = require("http"),
+	path = require("path"),
+	hogan = require("hogan-express"),
+	routes = require("./routes"),
+	scriptEnumerator = require("./lib/script-enumerator"),
+	config = require("./../../config");
+
+var assetsDir = path.join(__dirname + "/../client");
+
+var app = express();
+module.exports = app;
+
+app.configure(function () {
+	app.set("port", config.port || 1337);
+	app.set("databasePath", config.databasePath || path.join(__dirname + "/../../databases"));
+
+	app.locals({
+		title: "Keepass Web GUI"
+	});
+});
+
+app.configure(function () {
+	app.set("views", __dirname + "/views");
+	app.set("view engine", "html");
+	app.engine("html", hogan);
+
+	app.use(express.favicon());
+
+	app.use(express.logger("dev"));
+
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: "keyboard cat" }));
+
+	app.use("/assets", express.static(assetsDir));
+
+	app.use(scriptEnumerator(assetsDir));
+	app.use(app.router);
+
+	app.use(function logErrors(err, req, res, next) {
+		console.error(err ? err.stack || err : err);
+		next(err);
+	});
+
+	app.use(express.errorHandler());
+});
+
+routes.init(app);
--- src/server/index.js
+++ src/server/index.js
@@ -1,53 +1,5 @@
-﻿var express = require("express"),
-	http = require("http"),
-	path = require("path"),
-	hogan = require("hogan-express"),
-	routes = require("./routes"),
-	scriptEnumerator = require("./lib/script-enumerator"),
-	config = require("./../../config");
+﻿var app = require("./app");
 
-var assetsDir = path.join(__dirname + "/../client");
-
-var app = express();
-
-app.configure(function () {
-	app.set("port", config.port || 1337);
-	app.set("databasePath", config.databasePath || path.join(__dirname + "/../../databases"));
-
-	app.locals({
-		title: "Keepass Web GUI"
-	});
-});
-
-app.configure(function () {
-	app.set("views", __dirname + "/views");
-	app.set("view engine", "html");
-	app.engine("html", hogan);
-
-	app.use(express.favicon());
-
-	app.use(express.logger("dev"));
-
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: "keyboard cat" }));
-
-	app.use("/assets", express.static(assetsDir));
-
-	app.use(scriptEnumerator(assetsDir));
-	app.use(app.router);
-
-	app.use(function logErrors(err, req, res, next) {
-		console.error(err ? err.stack || err : err);
-		next(err);
-	});
-
-	app.use(express.errorHandler());
-});
-
-routes.init(app);
-
-http.createServer(app).listen(app.get("port"), function () {
+app.listen(app.get("port"), function () {
 	console.log("Now listening on port " + app.get("port"));
 });
