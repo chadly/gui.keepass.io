c95b4fde6f444c34b99c0204ea89d12191eec21b
Chad Lee <git@chadly.net>
2013-11-26 18:59:10 -0600

    Tweaking console messages.

--- src/server/index.js
+++ src/server/index.js
@@ -44,7 +44,7 @@ app.configure(function () {
 routes.init(app);
 
 http.createServer(app).listen(app.get("port"), function () {
-	console.log("Express server listening on port " + app.get("port"));
+	console.log("Now listening on port " + app.get("port"));
 });
 
 process.on("uncaughtException", function (err) {
