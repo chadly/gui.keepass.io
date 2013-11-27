2c72e73a22e83cea2612141378d6d0c35e66935d
Chad Lee <git@chadly.net>
2013-11-26 18:55:23 -0600

    Don't need folder name in api path.

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -3,28 +3,28 @@
     glob = require("glob");
 
 exports.init = function (app) {
-	app.get("/api/:folder", function (req, res) {
-		var folderPath = path.join(app.get("databasePath"), req.params.folder);
+	var dbPath = app.get("databasePath");
 
-		glob(path.join(folderPath, "/*.kdbx"), function (err, files) {
+	app.get("/api/", function (req, res) {
+		glob(path.join(dbPath, "/*.kdbx"), function (err, files) {
 			if (err) throw err;
 
 			res.send({
 				databases: files.map(function (file) {
-					return path.relative(folderPath, file).replace(/.kdbx/g, "");
+					return path.relative(dbPath, file).replace(/.kdbx/g, "");
 				})
 			});
 		});
 	});
 
-	app.get("/api/:folder/:name", function (req, res) {
+	app.get("/api/:name", function (req, res) {
 		var db = new Keepass();
 
 		db.setCredentials({
 			password: req.query.password
 		});
 
-		db.load(path.join(app.get("databasePath"), req.params.folder, req.params.name + ".kdbx"), function (err, data) {
+		db.load(path.join(dbPath, req.params.name + ".kdbx"), function (err, data) {
 			if (err) throw err;
 			res.send(data);
 		});
