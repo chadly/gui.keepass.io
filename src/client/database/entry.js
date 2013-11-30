8d57755979f6d11ee35a1b294dcb5fbcc43d5321
Chad Lee <git@chadly.net>
2013-11-30 12:53:20 -0600

    Adding entry controller with tests.

---
+++ src/client/database/entry.js
@@ -0,0 +1,7 @@
+﻿angular.module("keepass.io").controller("DatabaseEntryCtrl", function ($scope) {
+	var entry = $scope.selectedEntry();
+	//angular.copy freezes here…
+	for (var prop in entry) {
+		$scope[prop] = entry[prop];
+	}
+});
---
+++ src/client/database/entry.tests.js
@@ -0,0 +1,34 @@
+﻿//https://github.com/chaijs/chai/issues/41
+/* jshint expr:true */
+
+describe("Database Entry Controller", function () {
+	var expect = chai.expect, ctrl, scope;
+
+	beforeEach(angular.mock.module("keepass.io"));
+
+	beforeEach(angular.mock.inject(function ($rootScope, $controller) {
+		scope = $rootScope.$new();
+
+		scope.selectedEntry = function () {
+			return {
+				title: "Test Entry",
+				username: "homer.simpson",
+				password: "doh!"
+			};
+		};
+
+		ctrl = $controller("DatabaseEntryCtrl", { $scope: scope });
+	}));
+
+	describe("when creating new instance of controller", function () {
+		it("should create controller", function () {
+			expect(ctrl).not.to.be.undefined;
+		});
+
+		it("should copy entry data to scope", function () {
+			expect(scope.title).to.equal("Test Entry");
+			expect(scope.username).to.equal("homer.simpson");
+			expect(scope.password).to.equal("doh!");
+		});
+	});
+});
