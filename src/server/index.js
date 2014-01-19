dcc43dfd9c7416de3601e60f3a122e5d580bf5ba
Chad Lee <git@chadly.net>
2014-01-19 09:16:37 -0600

    Starting to implement SSL with PEM to autogenerate SSL keys

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,15 @@
-var app = require("./app");
+var app = require("./app"),
+	https = require("https"),
+	pem = require("pem");
 
-app.listen(app.get("port"), function () {
-	console.log("Now listening on port " + app.get("port"));
+pem.createCertificate({
+	days: 1,
+	selfSigned: true
+}, function (err, keys) {
+	https.createServer({
+		key: keys.serviceKey,
+		cert: keys.certificate
+	}, app).listen(app.get("port"), function () {
+		console.log("Now listening on port " + app.get("port"));
+	});
 });
