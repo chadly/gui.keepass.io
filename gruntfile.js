bb11cca5ecaeb94ea0bd09cb28ee84f9f99399c6
Chad Lee <git@chadly.net>
2014-01-20 16:48:21 -0600

    Include swf output in build

--- gruntfile.js
+++ gruntfile.js
@@ -105,6 +105,7 @@ module.exports = function (grunt) {
 						"client/**/*.ttf",
 						"client/**/*.woff",
 						"client/**/*.otf",
+						"client/**/*.swf"
 					],
 					dest: "build/"
 				}, {
