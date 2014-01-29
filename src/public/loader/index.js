dff8c392ab0ee043b6a303a300993e7d5615dfcb
Chad Lee <git@chadly.net>
2014-01-29 16:32:28 -0600

    If only single database available, redirect to it

--- src/public/loader/index.js
+++ src/public/loader/index.js
@@ -1,9 +1,13 @@
-angular.module("keepass.io").controller("LoaderCtrl", function ($scope, $rootScope, $routeParams, $http) {
+angular.module("keepass.io").controller("LoaderCtrl", function ($scope, $rootScope, $routeParams, $http, $location) {
 	$rootScope.cssScope = "kp-home";
 
 	$scope.type = $routeParams.type;
 
 	$http.get("/api/" + $routeParams.type).success(function (response) {
 		$scope.databases = response.databases;
+
+		if ($scope.databases.length === 1) {
+			$location.path($routeParams.type + "/" + $scope.databases[0].name);
+		}
 	});
 });
--- src/public/loader/tests.js
+++ src/public/loader/tests.js
@@ -9,19 +9,7 @@ describe("Loader Controller", function () {
 
 		beforeEach(angular.mock.module("keepass.io"));
 
-		beforeEach(angular.mock.inject(function ($httpBackend, $controller, $rootScope) {
-			$httpBackend.when("GET", "/api/local").respond({
-				databases: [{
-					name: "test1.kdbx",
-					size: 100,
-					modifiedOn: "2013-11-27"
-				}, {
-					name: "test2.kdbx",
-					size: 200,
-					modifiedOn: "2012-11-27"
-				}]
-			});
-
+		beforeEach(angular.mock.inject(function ($controller, $rootScope) {
 			scope = $rootScope.$new();
 			ctrl = $controller("LoaderCtrl", {
 				$scope: scope,
@@ -29,7 +17,6 @@ describe("Loader Controller", function () {
 					type: "local"
 				}
 			});
-			$httpBackend.flush();
 		}));
 
 		it("should create controller", function () {
@@ -39,13 +26,53 @@ describe("Loader Controller", function () {
 		it("should set loader type on scope", function () {
 			expect(scope.type).to.equal("local");
 		});
-		
-		it("should set list of databases from server on scope", function () {
-			expect(scope.databases).not.to.be.undefined;
-			expect(scope.databases.length).to.equal(2);
 
-			expect(scope.databases[0].name).to.equal("test1.kdbx");
-			expect(scope.databases[1].name).to.equal("test2.kdbx");
+		describe("with successful server response of multiple databases", function () {
+			beforeEach(angular.mock.inject(function ($httpBackend) {
+				$httpBackend.when("GET", "/api/local").respond({
+					databases: [{
+						name: "test1.kdbx",
+						size: 100,
+						modifiedOn: "2013-11-27"
+					}, {
+						name: "test2.kdbx",
+						size: 200,
+						modifiedOn: "2012-11-27"
+					}]
+				});
+
+				$httpBackend.flush();
+			}));
+
+			it("should set list of databases from server on scope", function () {
+				expect(scope.databases).not.to.be.undefined;
+				expect(scope.databases.length).to.equal(2);
+
+				expect(scope.databases[0].name).to.equal("test1.kdbx");
+				expect(scope.databases[1].name).to.equal("test2.kdbx");
+			});
+		});
+
+		describe("with successful server response with single database", function () {
+			var location;
+
+			beforeEach(angular.mock.inject(function ($httpBackend, $location) {
+				location = $location;
+
+				$httpBackend.when("GET", "/api/local").respond({
+					databases: [{
+						name: "test1.kdbx",
+						size: 100,
+						modifiedOn: "2013-11-27"
+					}]
+				});
+
+				$httpBackend.flush();
+			}));
+
+			it("should redirect to the single database", function () {
+				expect(location.path()).to.equal("/local/test1.kdbx");
+			});
 		});
 	});
 });
